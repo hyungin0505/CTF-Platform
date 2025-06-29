f9149f52954ca82ce2580b5f0a8fb1fe223067ee
hyungin0505 <hyungin0505@naver.com>
2025-06-29 21:26:15 +0900

    build: 프로젝트 이름 변경

--- src/test/java/com/ctfplatform/backend/CtfBackendApplicationTests.java
+++ src/test/java/com/ctfplatform/backend/CtfBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ctfplatform.CTF_Platform;
+package com.ctfplatform.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CtfPlatformApplicationTests {
+class CtfBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
