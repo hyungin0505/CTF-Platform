0b938dfbacbb3f4e8ef300a9bb4fea43d383015b
hyungin0505 <hyungin0505@naver.com>
2025-06-29 22:03:15 +0900

    feat: user 테이블 엔티티
    role, country, status, country 열거형

---
+++ src/main/java/com/ctfplatform/backend/domain/User.java
@@ -0,0 +1,58 @@
+package com.ctfplatform.backend.domain;
+
+import com.ctfplatform.backend.domain.common.BaseEntity;
+import com.ctfplatform.backend.domain.enums.Country;
+import com.ctfplatform.backend.domain.enums.Role;
+import com.ctfplatform.backend.domain.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30, unique = true)
+    private String nickname;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 32)
+    private Country country;
+
+    @Column(nullable = false)
+    private LocalDate birth;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    @Column(nullable = false, length = 10)
+    private Role role = Role.LEADER;
+
+    @Column(length = 50)
+    private String link;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Integer points = 0;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    @Column(nullable = false, length = 10)
+    private Status status = Status.INACTIVE;
+
+    @Column(nullable = false, length = 100)
+    private String passwordHash;
+}
