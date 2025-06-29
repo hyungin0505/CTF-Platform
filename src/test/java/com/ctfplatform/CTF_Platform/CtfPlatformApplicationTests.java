d2b4686f9497caecd110db6fd0edd47947620c39
hyungin0505 <hyungin0505@naver.com>
2025-06-29 21:02:01 +0900

    Initial commit

---
+++ src/test/java/com/ctfplatform/CTF_Platform/CtfPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctfplatform.CTF_Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtfPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
