2ee9e6d62df1a95d58234f45a35f02589f807301
hyungin0505 <hyungin0505@naver.com>
2025-07-09 03:02:22 +0900

    feat: JpaRepository 상속 설정

--- src/main/java/com/ctfplatform/backend/domain/team/repository/TeamRepository.java
+++ src/main/java/com/ctfplatform/backend/domain/team/repository/TeamRepository.java
@@ -1,10 +1,11 @@
 package com.ctfplatform.backend.domain.team.repository;
 
 import com.ctfplatform.backend.domain.team.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface TeamRepository extends PagingAndSortingRepository<Team, Long> {
+public interface TeamRepository extends JpaRepository<Team, Long> {
     List<Team> findAllByOrderByPointsDesc();
 }
