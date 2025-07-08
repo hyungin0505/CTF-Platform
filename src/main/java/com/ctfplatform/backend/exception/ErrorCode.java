eac6fa9d05c1bfadcaf9d39ae1bb80ff88be9f78
hyungin0505 <hyungin0505@naver.com>
2025-07-09 01:25:32 +0900

    feat: 에러코드 사용자 찾을 수 없음 추가

--- src/main/java/com/ctfplatform/backend/exception/ErrorCode.java
+++ src/main/java/com/ctfplatform/backend/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     CHALLENGE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 문제를 찾을 수 없습니다."),
-    ALREADY_SOLVED(HttpStatus.BAD_REQUEST, "이미 푼 문제입니다.")
+    ALREADY_SOLVED(HttpStatus.BAD_REQUEST, "이미 푼 문제입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
 
     ;
 
